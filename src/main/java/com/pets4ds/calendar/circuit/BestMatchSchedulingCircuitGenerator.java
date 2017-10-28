cc3286c2ff6b53927a2b9d194ff302f40f244760
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
2017-10-28 21:29:27 +0200

    Modified BestMatchSchedulingCircuitGenerator such that optional weights can be specified to prioritize certain parties.

--- src/main/java/com/pets4ds/calendar/circuit/BestMatchSchedulingCircuitGenerator.java
+++ src/main/java/com/pets4ds/calendar/circuit/BestMatchSchedulingCircuitGenerator.java
@@ -6,14 +6,37 @@
 package com.pets4ds.calendar.circuit;
 
 import com.pets4ds.calendar.circuit.primitives.*;
+import java.util.Arrays;
 
 /**
  *
  * @author Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@rwth-aachen.de>
  */
 public class BestMatchSchedulingCircuitGenerator extends SchedulingCircuitGenerator {
+    private int[] _partyWeights;
+    
     public BestMatchSchedulingCircuitGenerator(int numberOfParties, int numberOfSlots) {
+        this(numberOfParties, numberOfSlots, createDefaultWeights(numberOfParties));
+    }
+    
+    public BestMatchSchedulingCircuitGenerator(int numberOfParties, int numberOfSlots, int[] partyWeights) {
         super(numberOfParties, numberOfSlots);
+        
+        if(partyWeights.length != numberOfParties)
+            throw new IllegalArgumentException("Number of weights does not match the number of parties.");
+        
+        for(int partyId = 0; partyId < numberOfParties; ++partyId) {
+            if(partyWeights[partyId] <= 0)
+                throw new IllegalArgumentException("Party weights must be positive.");
+        }
+        
+        _partyWeights = Arrays.copyOf(partyWeights, numberOfParties);
+    }
+    
+    private static int[] createDefaultWeights(int numberOfParties) {
+        int[] defaultWeights = new int[numberOfParties];
+        Arrays.fill(defaultWeights, 1);
+        return defaultWeights;
     }
 
     @Override
@@ -22,7 +45,15 @@ public class BestMatchSchedulingCircuitGenerator extends SchedulingCircuitGenera
         final Comparison comparison = new Comparison(builder);
         final Multiplex multiplex = new Multiplex(builder);
         
-        final int countLimit = getNumberOfParties();
+        final ConstantMultiply[] weightMultiplication = new ConstantMultiply[getNumberOfParties()];
+        int countLimit = 0;
+        
+        for(int partyId = 0; partyId < getNumberOfParties(); ++partyId) {
+            int weight = _partyWeights[partyId];
+            weightMultiplication[partyId] = new ConstantMultiply(builder, weight);
+            countLimit += weight;
+        }
+        
         final int requiredBits = Integer.SIZE - Integer.numberOfLeadingZeros(countLimit);
         
         final Wire zero = builder.xor(partyInputWires[0][0], partyInputWires[0][0]);
@@ -30,13 +61,14 @@ public class BestMatchSchedulingCircuitGenerator extends SchedulingCircuitGenera
         
         Wire[][] counts = new Wire[getNumberOfSlots()][];
         for(int slotId = 0; slotId < getNumberOfSlots(); ++slotId)
-            counts[slotId] = WireBlock.expand(partyInputWires[0][slotId], zero, requiredBits);
+            counts[slotId] = weightMultiplication[0].apply(WireBlock.expand(partyInputWires[0][slotId], zero, requiredBits));
         
         for(int partyId = 1; partyId < getNumberOfParties(); ++partyId) {
             for(int slotId = 0; slotId < getNumberOfSlots(); ++slotId) {
                 counts[slotId] = addition.apply(
                     counts[slotId],
-                    WireBlock.expand(partyInputWires[partyId][slotId], zero, requiredBits));
+                    weightMultiplication[partyId].apply(WireBlock.expand(partyInputWires[partyId][slotId], zero, requiredBits))
+                );
             }
         }
         
